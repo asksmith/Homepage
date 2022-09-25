8a1e049a8dadca5f6801761e77ea3bc9031232bd
asksmith <smith@ask-me.ru>
2022-09-26 00:16:49 +0200

    Add URL verification

--- src/controllers/bookmark/bookmarkSearcherController.js
+++ src/controllers/bookmark/bookmarkSearcherController.js
@@ -1,15 +1,24 @@
 import { addBookmarkItem } from "../../helpers/storage.js"
 import { bindBookmarkItemView } from "../../binders/bookmark/bindBookmarkItemView.js"
 import { getBookmarkItemView } from "../../views/bookmark/getBookmarkItemView.js"
+import { isValidURL } from "../../helpers/bookmark/isValidURL.js"
 
 export const bookmarkSearcherController=(eventName,data,ref)=>{
     if (eventName==='add'){
-        const element=getBookmarkItemView(data).then((element)=>{
-            addBookmarkItem(data);
-            bindBookmarkItemView(element);
-            ref.value='';
-        });
-        
-
+        if (isValidURL(data)){
+            const element=getBookmarkItemView(data).then((element)=>{
+                addBookmarkItem(data);
+                bindBookmarkItemView(element);
+                ref.value='';
+            });
+        }else{
+            const notification=document.createElement('div');
+            notification.className='notification';
+            notification.innerHTML='Address should be an URL<br>http://www.microsoft.com/';
+            ref.parentNode.appendChild(notification);
+            setTimeout(()=>{
+                notification.parentNode.removeChild(notification)
+            },2000)
+        }
     }
 }
